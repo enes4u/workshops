8721dbd093271994c865ca817cf56f8941b081dc
Muzun <muzun@pgh.yearup.org>
2024-12-11 03:33:30 -0500

    addSalesContract method implemented

--- src/main/java/com/yearup/dealership/db/SalesDao.java
+++ src/main/java/com/yearup/dealership/db/SalesDao.java
@@ -16,5 +16,24 @@ public class SalesDao {
 
     public void addSalesContract(SalesContract salesContract) {
         // TODO: Implement the logic to add a sales contract
+        String sql = "INSERT INTO sales_contracts (VIN, sale_date, price) VALUES (?, ?, ?)";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setString(1, salesContract.getVin());
+            preparedStatement.setDate(2, Date.valueOf(salesContract.getSaleDate()));
+            preparedStatement.setDouble(3, salesContract.getPrice());
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Sales contract successfully added.");
+            } else {
+                System.out.println("Failed to add sales contract.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
