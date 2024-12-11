1dbdf59e9c8e0ee08d526401facb5c22ebb6aa85
Muzun <muzun@pgh.yearup.org>
2024-12-11 03:26:12 -0500

    InventoryDAO built

---
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -0,0 +1,45 @@
+package com.yearup.dealership.db;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class InventoryDao {
+    private DataSource dataSource;
+
+    public InventoryDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void addVehicleToInventory(String vin, int dealershipId) {
+        // TODO: Implement the logic to add a vehicle to the inventory
+        String sql = "INSERT INTO inventory (dealership_id, VIN) VALUES (?, ?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, dealershipId);
+            preparedStatement.setString(2, vin);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeVehicleFromInventory(String vin) {
+        // TODO: Implement the logic to remove a vehicle from the inventory
+        String sql = "DELETE FROM inventory WHERE VIN = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, vin);
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected == 0) {
+                System.out.println("No vehicle found with VIN: " + vin);
+            } else {
+                System.out.println("Vehicle with VIN " + vin + " has been removed from the inventory.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
