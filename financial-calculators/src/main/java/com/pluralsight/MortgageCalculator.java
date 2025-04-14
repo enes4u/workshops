f3d0ba4e005774c69feff6624bc2aebe0293ea93
enes4u <enesuzunyearup@gmail.com>
2025-04-14 09:11:31 -0400

    workshop project 1 hope works, well it works on my computer lol

---
+++ financial-calculators/src/main/java/com/pluralsight/MortgageCalculator.java
@@ -0,0 +1,38 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class MortgageCalculator {
+    public static void main(String[] args) {
+        Scanner realtor = new Scanner(System.in);
+
+        //user input
+        System.out.println(" Enter the loan principal amount: ");
+        double principal = realtor.nextDouble();
+
+        System.out.println(" Enter the annual interest rate ( as a percentage, e.g., 7.625 or 5.125 ");
+        double annualRate = realtor.nextDouble()/100; // convert to decimal
+
+        System.out.println(" Enter the loan term (in years): ");
+        int years = realtor.nextInt();
+
+        // monthly interes and number of payments
+        double monthlyRate = annualRate/12;
+        int numPayments = years* 12;
+
+        //calculation
+        double monthlyPayment = principal *
+                (monthlyRate * Math.pow(1 + monthlyRate, numPayments)) /
+                (Math.pow(1 + monthlyRate, numPayments) - 1);
+
+        //total interest
+        double totalInterest = (monthlyPayment* numPayments) - principal;
+
+        // results yayyy!
+        System.out.printf(" Your monthly payment is $%.2f%n",monthlyPayment );
+        System.out.printf("Total interest paid over the loan term: $%.2f%n",totalInterest );
+        realtor.close();// realtor no longer needed ;))
+
+
+    }
+}
