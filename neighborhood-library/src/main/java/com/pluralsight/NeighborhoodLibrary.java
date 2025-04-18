d4f9e0ae7cf5841370c5ba573ec67e84df8efb6a
enes4u <enesuzunyearup@gmail.com>
2025-04-18 12:23:45 -0400

    getters setters done will work on arrays and main method

---
+++ neighborhood-library/src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class NeighborhoodLibrary {
+    public static void main(String[] args) {
+
+
+        Scanner libClerk = new Scanner(System.in);
+        boolean running = true;
+
+    }
+}
